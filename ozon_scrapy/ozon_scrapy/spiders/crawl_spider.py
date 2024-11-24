a4ff6eb7445e37ef5590e354c6a7896424bac2e6
aleksey.matveev <aleksey.matveev@alarislabs.com>
2024-11-25 00:18:49 +0300

    add scrapy-splash

--- ozon_scrapy/ozon_scrapy/spiders/crawl_spider.py
+++ ozon_scrapy/ozon_scrapy/spiders/crawl_spider.py
@@ -1,6 +1,21 @@
 import scrapy
+from scrapy.http import Response
+from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from scrapy_splash import SplashRequest
 
 
-class OzonCrawlSpider(CrawlSpider):
-    pass
+class OzonCrawlSpider(scrapy.Spider):
+    name = "extract_links"
+    start_urls = ["https://www.ozon.ru/"]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield SplashRequest(url, self.parse, args={"wait": 2})
+
+    def parse(self, response: Response):
+        all_links = response.css("a::attr(href)").getall()
+        filtered_links = [
+            response.urljoin(link) for link in all_links if link.startswith("/product/")
+        ]
+        print(filtered_links)
